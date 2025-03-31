63be3a819bc41da75290640a968bb7fd99b733c5
Drew Twitchell <19315286+drewtwitchell@users.noreply.github.com>
2025-03-31 15:58:49 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -94,16 +94,13 @@ if "%1"=="--uninstall" (
 function Create-MacWrapperScript {
     param($InstallDir)
     $wrapperPath = "$InstallDir/scancompare"
-    $pythonPath = "$InstallDir/venv/bin/python3"
-    $scriptPath = "$InstallDir/scancompare"
-
     $lines = @(
         "#!/bin/bash",
         'if [ "$1" = "--uninstall" ]; then',
         '    pwsh -Command ''$script = "$HOME/ScanCompare/install.ps1"; & $script --uninstall''',
-        "    exit 0",
+        '    exit 0',
         "fi",
-        "$pythonPath \"$scriptPath\" \"\$@\""
+        "$InstallDir/venv/bin/python3 \"$InstallDir/scancompare\" \"\$@\""
     )
     Set-Content -Path $wrapperPath -Value $lines -Encoding UTF8
     chmod +x $wrapperPath
